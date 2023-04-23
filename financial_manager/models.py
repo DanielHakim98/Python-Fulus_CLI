3f0e4805a32987e04f037d277ce96889507b179e
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-04-23 17:10:04 +0800

    Fix issue related to integration with alembic

--- financial_manager/models.py
+++ financial_manager/models.py
@@ -12,21 +12,25 @@ from sqlalchemy import (
     String,
     Float,
     DateTime,
-    Column,
     ForeignKey,
-    extract,
+    UniqueConstraint
 )
 
 class Base(DeclarativeBase):
     pass
 
+
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "transaction_user"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String)
 
-    transactions: Mapped[list["Transaction"] | None] = relationship(back_populates = "user")
+    transactions: Mapped[list["Transaction"] | None] = relationship(back_populates = "transaction_user")
+
+    __table_args__ = (
+        UniqueConstraint('name', 'email', name='uq_user_name_email'),
+    )
 
     def __init__(self, name: str, email:str) -> None:
         self.name = name
@@ -36,11 +40,11 @@ class User(Base):
         return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
 
 class Category(Base):
-    __tablename__ = "category"
+    __tablename__ = "transaction_category"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(String)
 
-    transactions: Mapped[list["Transaction"] | None] = relationship(back_populates = "category")
+    transactions: Mapped[list["Transaction"] | None] = relationship(back_populates="transaction_category")
 
     def __init__(self, title: str) -> None:
         self.title = title
@@ -48,13 +52,14 @@ class Category(Base):
     def __repr__(self) -> str:
         return f"Category(id={self.id!r}, title={self.title!r})"
 
+
 class Transaction(Base):
     __tablename__ = "transaction_master"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     date: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
     amount: Mapped[float ]= mapped_column(Float, default = 0.0)
-    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey("transaction_category.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("transaction_user.id"), nullable=False)
 
     category: Mapped[Category] = relationship(back_populates = "transaction_master")
     user: Mapped[User] = relationship(back_populates = "transaction_master")
@@ -66,7 +71,13 @@ class Transaction(Base):
         self.user_id = user_id
 
     def __repr__(self) -> str:
-        return f"Transaction(id={self.id!r}, date={self.date!r}, amount={self.amount!r}, category_id={self.category_id!r}, user_id={self.user_id!r})"
+        return (
+            f"Transaction(id={self.id!r}"
+            + f"date={self.date!r}"
+            + f"amount={self.amount!r}"
+            + f"category_id={self.category_id!r}"
+            + f"user_id={self.user_id!r})"
+        )
 
 def convert_to_datetime(date:str)-> datetime | int:
     pattern = r"^(19\d{2}|20[0-2][0-9])-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
@@ -74,3 +85,5 @@ def convert_to_datetime(date:str)-> datetime | int:
         split = date.split('-')
         return datetime(split[0],split[1],split[2])
     return FILE_ERR
+
+print(Base.metadata.tables)
