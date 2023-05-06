0b488606b27f7916def42fd4eb731f87d1073c4f
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-05-06 14:44:26 +0800

    Add help for 'users' and 'categories' command

--- fulus_cli/cli/__init__.py
+++ fulus_cli/cli/__init__.py
@@ -2,5 +2,5 @@ import typer
 from fulus_cli.cli import users, categories
 
 app = typer.Typer()
-app.add_typer(users.app, name="users")
-app.add_typer(categories.app, name="categories")
+app.add_typer(users.app, name="users", help= "Show all 'users' sub-commands")
+app.add_typer(categories.app, name="categories", help= "Show all 'categories' sub-commands")
