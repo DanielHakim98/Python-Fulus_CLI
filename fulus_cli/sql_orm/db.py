3f62d02a512bba23541d1da44c12b08f7108b45d
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-05-06 11:35:30 +0800

    Add 'get_id' method in DBConnection

--- fulus_cli/sql_orm/db.py
+++ fulus_cli/sql_orm/db.py
@@ -9,6 +9,27 @@ class DBConnection():
         self.engine = sql.create_engine(db_path)
         self.session = Session(self.engine)
 
+    def get_id(self, model_obj)\
+            -> tuple[Sequence[models.Base] | None, int]:
+        if isinstance(model_obj, models.User):
+            stmt = select(models.User)\
+                .where(
+                    models.User.name == model_obj.name
+                ).where(
+                    models.User.email == model_obj.email
+                )
+        elif isinstance(model_obj, models.Category):
+            stmt = select(models.Category).where(
+                models.Category.title == model_obj.title
+            )
+
+        try:
+            with self.session as session:
+                result = session.scalars(stmt).all()
+            return result, 0
+        except Exception as e:
+            return None, DB_READ_ERR
+
     def create(self, model_obj: models.Base) -> int:
         try:
             with self.session as session:
@@ -18,18 +39,19 @@ class DBConnection():
         except Exception as e:
             return DB_WRITE_ERR
 
-    def get_all(self, model_obj) -> tuple[Sequence[models.User] | None, int]:
+    def read(self, model_obj) -> tuple[Sequence[models.Base] | None, int]:
         try:
             with self.session as session:
                 result = session.scalars(select(model_obj)).all()
             return result, 0
         except Exception as e:
-            return DB_READ_ERR
+            return None,DB_READ_ERR
 
-    def delete(self, model_obj) -> int:
+    def delete(self, model_obj, id) -> int:
         try:
+            stmt = delete(model_obj).where(model_obj.id == id)
             with self.session as session:
-                session.delete(model_obj)
+                session.execute(stmt)
                 session.commit()
             return SUCCESS
         except Exception as e:
@@ -52,44 +74,6 @@ def init_database(db_path: str) -> int:
         return SUCCESS
     except Exception as e:
         return DB_WRITE_ERR
-
-def create_user(db_path: str, name: str, email: str) -> int:
-    """Create a new user from the given name"""
-    try:
-        engine = sql.create_engine(db_path)
-        user = models.User(name=name, email=email)
-        with Session(engine) as session:
-            session.add(user)
-            session.commit()
-        return SUCCESS
-
-    except Exception as e:
-        return DB_WRITE_ERR
-
-def remove_user(db_path: str, name: str, email: str) -> int:
-    """Remove a user from the given name and email"""
-    try:
-        engine = sql.create_engine(db_path)
-        stmt = delete(models.User)\
-                .where(models.User.name == name)\
-                .where(models.User.email == email)
-        with Session(engine) as session:
-            session.execute(stmt)
-            session.commit()
-        return SUCCESS
-
-    except Exception as e:
-        return DB_WRITE_ERR
-
-def list_users(db_path: str) -> tuple[Sequence[models.User] | None, int]:
-    """Get list of users created"""
-    try:
-        engine = sql.create_engine(db_path)
-        with Session(engine) as session:
-            result = session.scalars(select(models.User)).all()
-        return result, 0
-    except Exception as e:
-        return None, DB_READ_ERR
 
 def create_category(db_path: str, title: str) -> int:
     pass
