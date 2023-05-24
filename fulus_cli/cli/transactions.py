2d330332e9f7ada295531fd4b24cb3ccb214bf2e
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-05-24 22:21:53 +0800

    Add: add 'remove' command in transactions.py and unit test for the command

--- fulus_cli/cli/transactions.py
+++ fulus_cli/cli/transactions.py
@@ -10,24 +10,21 @@ from fulus_cli.sql_orm.models import convert_to_datetime as to_dt
 app = typer.Typer()
 database = db.DBConnection(Config.SQLALCHEMY_DATABASE_URI)
 
+
 def _validate_date(date: str) -> datetime.datetime:
     date_datetime, status_code = to_dt(date)
     if status_code != 0:
         typer.secho(
-            f"Date does not conform to the expected format.",
-            fg=typer.colors.RED
+            f"Date does not conform to the expected format.", fg=typer.colors.RED
         )
         raise typer.Exit(1)
     return date_datetime
 
+
 def _validate_amount(amount: str) -> str:
-    if not re.fullmatch(
-        r"^(?:[1-9]\d*|0)(?:\.\d+)?$",
-        amount
-    ):
+    if not re.fullmatch(r"^(?:[1-9]\d*|0)(?:\.\d+)?$", amount):
         typer.secho(
-            "Amount must be either more than zero and a number",
-            fg=typer.colors.RED
+            "Amount must be either more than zero and a number", fg=typer.colors.RED
         )
         raise typer.Exit(1)
     return amount
@@ -38,81 +35,79 @@ def _category_id(category: str) -> int:
     if status_code != 0:
         typer.secho(
             f"Category can't be retrieved. Error: {ERRORS.get(status_code)}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
         )
         raise typer.Exit(1)
     elif len(category_list) < 1:
-        typer.secho(
-            f"Category does not exist",
-            fg=typer.colors.RED
-        )
+        typer.secho(f"Category does not exist", fg=typer.colors.RED)
         raise typer.Exit(1)
     return category_list[0].id
+
 
 def _user_id(user: str) -> int:
     user_list, status_code = database.get_id(models.User(name=user, email=""))
     if status_code != 0:
         typer.secho(
             f"User can't be retrieved. Error: {ERRORS.get(status_code)}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
         )
         raise typer.Exit(1)
     elif len(user_list) < 1:
-        typer.secho(
-            f"User does not exist",
-            fg=typer.colors.RED
-        )
+        typer.secho(f"User does not exist", fg=typer.colors.RED)
         raise typer.Exit(1)
     return user_list[0].id
 
 
 @app.command()
 def create(
-    date: str = typer.Argument(...,
-                               help="The date spent (in 'YY-MM-DD' format)",
-                               callback=_validate_date),
-    amount: str = typer.Argument(...,
-                                 help="Money spent in currenciless unit",
-                                 callback=_validate_amount),
+    date: str = typer.Argument(
+        ..., help="The date spent (in 'YY-MM-DD' format)", callback=_validate_date
+    ),
+    amount: str = typer.Argument(
+        ..., help="Money spent in currenciless unit", callback=_validate_amount
+    ),
     category: str = typer.Argument(..., help="Category name", callback=_category_id),
     user: str = typer.Argument(..., help="User name", callback=_user_id),
 ):
     """Insert a transaction"""
 
     transaction = models.Transaction(
-        date=date,
-        amount=float(amount),
-        category_id=category,
-        user_id=user
+        date=date, amount=float(amount), category_id=category, user_id=user
     )
 
     status_code = database.create(transaction)
     if status_code != 0:
         typer.secho(
             f"Transaction insertion failed. Error {ERRORS[status_code]}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
         )
         raise typer.Exit(1)
     else:
-        typer.secho(
-            f"Transaction has been inserted.",
-            fg=typer.colors.GREEN
-        )
+        typer.secho(f"Transaction has been inserted.", fg=typer.colors.GREEN)
+
 
 @app.command()
-def delete():
-    pass
+def delete(tx_id: str = typer.Argument(..., help="The name of the category")):
+    """Remove existing transaction"""
+    status_code = database.delete(models.Transaction, tx_id)
+    if status_code != 0:
+        typer.secho(
+            f"Transaction can't be removed. Error: {ERRORS[status_code]}",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"Transaction ID '{tx_id}' has been removed", fg=typer.colors.GREEN)
+
 
 @app.command()
 def list():
     """List all transactions"""
-    result, status_code = database.read(
-        models.Transaction
-    )
+    result, status_code = database.read(models.Transaction)
     if status_code != 0:
         typer.secho(
             f"Failed while retriveing users. Error: {ERRORS[status_code]}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
         )
         raise typer.Exit(1)
     else:
@@ -120,15 +115,18 @@ def list():
         typer.secho("id | date | amount | category_id | user_id | ")
         typer.secho("--------------------------------------------")
         for row in result:
-            typer.secho(' | '.join(
-                [
-                    str(row.id),
-                    row.date.strftime('%Y-%m-%d'),
-                    str(row.amount),
-                    str(row.category_id),
-                    str(row.user_id)
-                ]
-            ))
+            typer.secho(
+                " | ".join(
+                    [
+                        str(row.id),
+                        row.date.strftime("%Y-%m-%d"),
+                        str(row.amount),
+                        str(row.category_id),
+                        str(row.user_id),
+                    ]
+                )
+            )
+
 
 if __name__ == "__main__":
     app()
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,34 +1,46 @@
-from datetime import datetime
 import pytest
+from datetime import datetime
 from config import Config
-from unittest import mock
-from fulus_cli.cli import main
+from fulus_cli.cli import main, transactions
+from fulus_cli.cli.transactions import _user_id
 from typer.testing import CliRunner
 from fulus_cli.sql_orm import db, models
 
 runner = CliRunner()
 
+
 class TestTransaction:
     date = "2023-05-23"
     amount = "100"
-    category= "Food"
-    user = "Daniel"
+    category = "Food"
+    user = "Danil"
     db_path = Config.SQLALCHEMY_DATABASE_URI
 
     def test_create_transaction(self, monkeypatch):
-        def mocked_create(mocked_self, mocked_obj):
+        def mocked_id(self, mocked_obj):
+            if isinstance(mocked_obj, models.Category):
+                return ([models.Category(title=TestTransaction.category)], 0)
+            elif isinstance(mocked_obj, models.User):
+                return ([models.User(name=TestTransaction.user, email="")], 0)
+            else:
+                raise ValueError("Invalid object type")
+
+        def mocked_create(self, mocked_obj):
             return 0
-        monkeypatch.setattr(db.DBConnection, 'create', mocked_create)
+
+        monkeypatch.setattr(db.DBConnection, "get_id", mocked_id)
+        monkeypatch.setattr(db.DBConnection, "create", mocked_create)
         result = runner.invoke(
             main.app,
             [
-                "transactions", "create",
+                "transactions",
+                "create",
                 TestTransaction.date,
                 TestTransaction.amount,
                 TestTransaction.category,
                 TestTransaction.user,
             ],
-            env={"SQLALCHEMY_DATABASE_URI": TestTransaction.db_path}
+            env={"SQLALCHEMY_DATABASE_URI": TestTransaction.db_path},
         )
 
         print("Exit code:", result.exit_code)
@@ -37,43 +49,46 @@ class TestTransaction:
         assert result.exit_code == 0
         assert "Transaction has been inserted." in result.stdout
 
-
     def test_list_transaction(self, monkeypatch):
         def mocked_get_all(self, model_object):
             fake_data = [
                 models.Transaction(
-                    date=datetime(2020, 2, 2),
-                    amount=100,
-                    category_id=1,
-                    user_id=1
+                    date=datetime(2020, 2, 2), amount=100, category_id=1, user_id=1
                 ),
                 models.Transaction(
-                    date=datetime(2021, 9, 1),
-                    amount=9.9,
-                    category_id=2,
-                    user_id=1
+                    date=datetime(2021, 9, 1), amount=9.9, category_id=2, user_id=1
                 ),
                 models.Transaction(
-                    date=datetime(2023, 3, 13),
-                    amount=1000,
-                    category_id=3,
-                    user_id=2
-                )
+                    date=datetime(2023, 3, 13), amount=1000, category_id=3, user_id=2
+                ),
             ]
             return fake_data, 0
-        monkeypatch.setattr(db.DBConnection, 'read', mocked_get_all)
-        result =runner.invoke(
-            main.app,
-            ["transactions", "list"]
+
+        monkeypatch.setattr(db.DBConnection, "read", mocked_get_all)
+        result = runner.invoke(main.app, ["transactions", "list"])
+        expected_output = (
+            "--------------------------------------------\n"
+            "id | date | amount | category_id | user_id | \n"
+            "--------------------------------------------\n"
+            "None | 2020-02-02 | 100 | 1 | 1\n"
+            "None | 2021-09-01 | 9.9 | 2 | 1\n"
+            "None | 2023-03-13 | 1000 | 3 | 2"
         )
-        expected_output = "--------------------------------------------\n" \
-                "id | date | amount | category_id | user_id | \n" \
-                "--------------------------------------------\n" \
-                "None | 2020-02-02 | 100 | 1 | 1\n" \
-                "None | 2021-09-01 | 9.9 | 2 | 1\n"\
-                "None | 2023-03-13 | 1000 | 3 | 2"
         assert result.output.strip() == expected_output
 
     def test_remove_transaction(self, monkeypatch):
-        pass
+        tx_id = "100"
 
+        def mocked_delete(self, mocked_obj, id):
+            return 0
+
+        monkeypatch.setattr(db.DBConnection, "delete", mocked_delete)
+        result = runner.invoke(
+            main.app,
+            ["transactions", "delete", tx_id],
+            env={"SQLALCHEMY_DATABASE_URI": TestTransaction.db_path},
+        )
+        print("Exit code:", result.exit_code)
+        print("Result stdout:", result.stdout)
+        assert result.exit_code == 0
+        assert f"Transaction ID '{tx_id}' has been removed" in result.stdout
