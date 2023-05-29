4906244bddb77bcb487317fcd5787b914aa7bb73
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2023-05-29 21:46:44 +0800

    Create: implement 'update' command in categories.py and its unit test

--- fulus_cli/cli/categories.py
+++ fulus_cli/cli/categories.py
@@ -1,54 +1,37 @@
 """CLI commands for 'Categories'"""
+from typing import Annotated, Optional
 import typer
 import re
 from config import Config
-from fulus_cli import (
-    __app_name__,
-    __version__,
-    ERRORS
-)
+from fulus_cli import __app_name__, __version__, ERRORS
 from fulus_cli.sql_orm import db, models
 
 app = typer.Typer()
 database = db.DBConnection(Config.SQLALCHEMY_DATABASE_URI)
 
+
 @app.command()
-def create(
-    title: str = typer.Argument(..., help="The name of the category")
-) -> None:
+def create(title: str = typer.Argument(..., help="The name of the category")) -> None:
     """Create a new category"""
     # Check if title is empty
     if not title.strip():
-        typer.secho(
-            "Name cannot be empty",
-            fg=typer.colors.RED
-        )
+        typer.secho("Name cannot be empty", fg=typer.colors.RED)
         raise typer.Exit(1)
 
     # Check if the title is invalid (only characters and numbersa are allowed)
-    if not re.fullmatch(
-            r"[A-Za-z0-9]+",
-            title.strip()):
-        typer.secho(
-            "Title must be a valid title",
-            fg=typer.colors.RED
-        )
+    if not re.fullmatch(r"[A-Za-z0-9]+", title.strip()):
+        typer.secho("Title must be a valid title", fg=typer.colors.RED)
         raise typer.Exit(1)
 
-    category = models.Category(
-        title=title.strip()
-    )
+    category = models.Category(title=title.strip())
     status_code = database.create(category)
     if status_code != 0:
         typer.secho(
             f"Category creation failed. Error {ERRORS[status_code]}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
         )
     else:
-        typer.secho(
-            f"Category '{title}' has been added",
-            fg=typer.colors.GREEN
-        )
+        typer.secho(f"Category '{title}' has been added", fg=typer.colors.GREEN)
 
 
 @app.command()
@@ -58,7 +41,7 @@ def list() -> None:
     if status_code != 0:
         typer.secho(
             f"Failed while retriveing categories. Error: {ERRORS[status_code]}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
         )
         raise typer.Exit(1)
     else:
@@ -66,39 +49,69 @@ def list() -> None:
         typer.secho("id | title ")
         typer.secho("------------")
         for row in result:
-            typer.secho(' | '.join([str(row.id), row.title]))
+            typer.secho(" | ".join([str(row.id), row.title]))
+
 
 @app.command()
 def update(
+    category_id: str = typer.Argument(..., help="The id of the category"),
+    title: Annotated[
+        Optional[str], typer.Argument(..., help="New category title")
+    ] = None,
 ) -> None:
-    pass
-
-@app.command()
-def delete(
-    title: str = typer.Argument(..., help="The name of the category")
-) -> None:
-    """Remove existing category"""
-    category, status_code = database.get_id(models.Category(title=title))
-    if status_code != 0 or category is None:
-        typer.secho(
-            f"Category can't be removed. Error: {ERRORS[status_code]}",
-            fg=typer.colors.RED
-        )
+    """Update existing category"""
+    # Check if title is empty
+    if not title.strip():
+        typer.secho("Name cannot be empty", fg=typer.colors.RED)
         raise typer.Exit(1)
 
-    category_id = category[0].id
-    status_code = database.delete(
-        models.Category,
-        category_id
-    )
+    # Check if the title is invalid (only characters and numbersa are allowed)
+    if not re.fullmatch(r"[A-Za-z0-9]+", title.strip()):
+        typer.secho("Title must be a valid title", fg=typer.colors.RED)
+        raise typer.Exit(1)
+
+    data = {"id": category_id, "title": title}
+    # To allow optional and variadic arguments for multi update operations
+    clean_data = {k: v for k, v in data.items() if v is not None}
+
+    status_code = database.update(models.Category, clean_data)
+    if status_code != 0:
+        typer.secho(
+            f"Category can't be updated. Error: {ERRORS[status_code]}",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(
+            f"Category ID '{category_id}' has been updated", fg=typer.colors.GREEN
+        )
+
+
+@app.command()
+def delete(title: str = typer.Argument(..., help="The name of the category")) -> None:
+    """Remove existing category"""
+
+    category, status_code = database.get_id(models.Category(title=title))
     if status_code != 0:
         typer.secho(
             f"Category can't be removed. Error: {ERRORS[status_code]}",
-            fg=typer.colors.RED
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    elif len(category) < 1:
+        typer.secho("Category does not exist", fg=typer.colors.RED)
+    category_id = category[0].id
+
+    status_code = database.delete(models.Category, category_id)
+    if status_code != 0:
+        typer.secho(
+            f"Category can't be removed. Error: {ERRORS[status_code]}",
+            fg=typer.colors.RED,
         )
         raise typer.Exit(1)
     else:
         typer.secho(f"Category '{title}' has been removed", fg=typer.colors.GREEN)
+
 
 if __name__ == "__main__":
     app()
--- tests/test_categories.py
+++ tests/test_categories.py
@@ -3,37 +3,33 @@ from config import Config
 from fulus_cli.cli import main
 from typer.testing import CliRunner
 from fulus_cli.sql_orm import db, models
-from fulus_cli import (
-    DB_WRITE_ERR,
-    ERRORS,
-    __app_name__,
-    __version__
-)
+from fulus_cli import DB_WRITE_ERR, ERRORS, __app_name__, __version__
 
 runner = CliRunner()
 
+
 class TestCategory:
-    TITLE = 'category1'
+    TITLE = "category1"
     db_path = Config.SQLALCHEMY_DATABASE_URI
 
     def test_create_category(self, monkeypatch):
-        def mocked_create(mocked_self,mocked_obj):
+        def mocked_create(mocked_self, mocked_obj):
             return 0
-        monkeypatch.setattr(db.DBConnection, 'create', mocked_create)
+
+        monkeypatch.setattr(db.DBConnection, "create", mocked_create)
         result = runner.invoke(
-                main.app,
-                ["categories","create", TestCategory.TITLE],
-                env={"SQLALCHEMY_DATABASE_URI": TestCategory.db_path}
+            main.app,
+            ["categories", "create", TestCategory.TITLE],
+            env={"SQLALCHEMY_DATABASE_URI": TestCategory.db_path},
         )
         assert result.exit_code == 0
-        assert f"Category '{TestCategory.TITLE}' has been added" \
-            in result.stdout
+        assert f"Category '{TestCategory.TITLE}' has been added" in result.stdout
 
     def test_is_not_empty_title(self):
         TITLE = ""
         result = runner.invoke(
-           main.app,
-           ["categories","create", TITLE],
+            main.app,
+            ["categories", "create", TITLE],
         )
         assert result.exit_code == 1
         assert f"Name cannot be empty" in result.stdout
@@ -43,34 +39,54 @@ class TestCategory:
             fake_data = [
                 models.Category(title="Transport"),
                 models.Category(title="Food"),
-                models.Category(title="Saving")
+                models.Category(title="Saving"),
             ]
             return fake_data, 0
-        monkeypatch.setattr(db.DBConnection, 'read', mocked_get_all)
-        result = runner.invoke(
-            main.app,
-            ["categories", "list"]
+
+        monkeypatch.setattr(db.DBConnection, "read", mocked_get_all)
+        result = runner.invoke(main.app, ["categories", "list"])
+        expected_output = (
+            "------------\n"
+            "id | title \n"
+            "------------\n"
+            "None | Transport\n"
+            "None | Food\n"
+            "None | Saving"
         )
-        expected_output = "------------\n" \
-                "id | title \n" \
-                "------------\n" \
-                "None | Transport\n" \
-                "None | Food\n"\
-                "None | Saving"
         assert result.output.strip() == expected_output
 
     def test_remove_category(self, monkeypatch):
         def mocked_delete(self, model_obj, id):
             return 0
+
         def mocked_get_id(self, mode_obj):
             return [models.Category(title=TestCategory.TITLE)], 0
+
         monkeypatch.setattr(db.DBConnection, "delete", mocked_delete)
         monkeypatch.setattr(db.DBConnection, "get_id", mocked_get_id)
         result = runner.invoke(
             main.app,
             ["categories", "delete", TestCategory.TITLE],
-            env={"SQLALCHEMY_DATABASE_URI": TestCategory.db_path}
+            env={"SQLALCHEMY_DATABASE_URI": TestCategory.db_path},
         )
 
         assert result.exit_code == 0
         assert f"Category '{TestCategory.TITLE}' has been removed" in result.stdout
+
+    def test_update_category(self, monkeypatch):
+        category_id = "100"
+        title = "NewTitle"
+
+        def mocked_update(self, mocked_obj, id):
+            return 0
+
+        monkeypatch.setattr(db.DBConnection, "update", mocked_update)
+        result = runner.invoke(
+            main.app,
+            ["categories", "update", category_id, "--title", title],
+            env={"SQLALCHEMY_DATABASE_URI": TestCategory.db_path},
+        )
+        print("Exit code:", result.exit_code)
+        print("Result stdout:", result.stdout)
+        assert result.exit_code == 0
+        assert f"Category ID '{category_id}' has been updated" in result.stdout
